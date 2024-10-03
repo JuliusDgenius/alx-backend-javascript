2a1aea498c37fb1702017fcd02b42d537393c036
JuliusDgenius <ibejulius1@gmail.com>
2024-10-03 16:56:47 +0100

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-// Return a Promise using this prototype function getResponseFromAPI()
-
 export default function getResponseFromAPI () {
   return new Promise((resolve, reject) => {
 
