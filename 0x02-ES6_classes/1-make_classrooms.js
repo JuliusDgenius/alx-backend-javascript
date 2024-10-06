e773d6994301546451d9926cc74e5d88d3030990
JuliusDgenius <ibejulius1@gmail.com>
2024-10-06 18:59:35 +0100

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-export default function intializeRooms () {
+export default function intializeRooms() {
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
